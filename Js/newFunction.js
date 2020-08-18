b3365c1d4edcdf37e8e1e3e5e3ec7aab236d7977
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2020-08-18 08:04:54 +0100

    Delete newFunction.js

--- Js/newFunction.js
+++
@@ -1,9 +0,0 @@
-// function newFunction() {
-	const closeAll = document.getElementById("message-upload-file--clear");
-	closeAll.addEventListener("click", function () {
-		// e.preventDefault();
-		alert("here");
-	});
-// }
-
-
