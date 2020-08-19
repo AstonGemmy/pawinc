f8e777eb8e3ff2050ca017400260079f0e9d10c8
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2020-08-19 17:42:19 +0100

    Update PAW.js

--- Js/PAW.js
+++ Js/PAW.js
@@ -288,7 +288,7 @@ window.addEventListener('load', function () {
 	notificationButton.addEventListener('click', function () {
 		
 		if (window.Notification && Notification.permission === "granted") {
-			registration.serviceWorker(title, options);
+			registration.showNotification(title, options);
 			alert("Permission granted");
 		} else if (window.Notification && Notification.permission !== "denied") {
 			Notification.requestPermission(function (status) {
