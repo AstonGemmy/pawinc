ffab0f939eb2da29fa82d9690af706e596f1453b
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2020-08-19 09:47:51 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -86,3 +86,17 @@ self.addEventListener('fetch', function(event) {
       })
     );
 });
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: '/Images/Logo/PAW.png',
+    badge: 'Images/Logo/PAW.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
