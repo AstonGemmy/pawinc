f361db2d632d2b48926c773b5e8f123d825a84e0
AstonGemmy <52787496+AstonGemmy@users.noreply.github.com>
2020-08-19 11:33:18 +0100

    Update PAW.js

--- Js/PAW.js
+++ Js/PAW.js
@@ -270,60 +270,34 @@ handleMarquee();
 const notificationButton = document.getElementById('notificationTrigger');
 
 window.addEventListener('load', function () {
-  // At first, let's check if we have permission for notification
-  // If not, let's ask for it
-  if (window.Notification && Notification.permission !== "granted") {
+    if (window.Notification && Notification.permission !== "granted") {
     Notification.requestPermission(function (status) {
       if (Notification.permission !== status) {
         Notification.permission = status;
       }
     });
   }
-
-  notificationButton.addEventListener('click', function () {
-    // If the user agreed to get notified
-    // Let's try to send ten notifications
+	const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: '/Images/PAW.png',
+    badge: '/Images/PAW.png'
+  };
+  
+notificationButton.addEventListener('click', function () {
     if (window.Notification && Notification.permission === "granted") {
-      var i = 0;
-      // Using an interval cause some browsers (including Firefox) are blocking notifications if there are too much in a certain time.
-      var interval = window.setInterval(function () {
-        // Thanks to the tag, we should only see the "Hi! 9" notification 
-        var n = new Notification("Hi! " + i, {tag: 'soManyNotification'});
-        if (i++ == 9) {
-          window.clearInterval(interval);
-        }
-      }, 200);
-    }
-
-    // If the user hasn't told if he wants to be notified or not
-    // Note: because of Chrome, we are not sure the permission property
-    // is set, therefore it's unsafe to check for the "default" value.
-    else if (window.Notification && Notification.permission !== "denied") {
+     new Notification(title, options);       
+    } else if (window.Notification && Notification.permission !== "denied") {
       Notification.requestPermission(function (status) {
-        // If the user said okay
-        if (status === "granted") {
-          var i = 0;
-          // Using an interval cause some browsers (including Firefox) are blocking notifications if there are too much in a certain time.
-          var interval = window.setInterval(function () {
-            // Thanks to the tag, we should only see the "Hi! 9" notification 
-            var n = new Notification("Hi! " + i, {tag: 'soManyNotification'});
-            if (i++ == 9) {
-              window.clearInterval(interval);
-            }
-          }, 200);
-        }
-
-        // Otherwise, we can fallback to a regular modal alert
-        else {
+       if (status === "granted") {
+         new Notification(title, options);            
+        } else {
           alert("Hi!");
         }
-      });
+    } else {
+       alert("Hi!");
     }
-
-    // If the user refuses to get notified
-    else {
-      // We can fallback to a regular modal alert
-      alert("Hi!");
-    }
+	  
   });
+	
 });
