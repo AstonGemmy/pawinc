ead51bb75163e176a06ba39475fa839ced4d8295
AstonGemmy <astongemmy@gmail.com>
2020-08-19 22:07:56 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -95,5 +95,5 @@ self.addEventListener('push', function(event) {
     icon: '/Images/Logo/PAW.png',
     badge: '/Images/Logo/PAW.png'
   };
-  event.waitUntil(self.showNotification(title, options));
+  event.waitUntil(showNotification(title, options));
 });
